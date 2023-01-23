cc817a520932984f4d1db37c28f4fd9412302693
frontalice <mayonakaTV@me.com>
2023-01-23 22:42:42 +0900

    Revert "Additional Commit"
    This reverts commit 48541623dfc2594e4eaaa6b6d7b5f0f43c426f19.

--- api/app/controllers/api/v1/additional_controller.rb
+++
@@ -1,9 +0,0 @@
-module Api
-  module V1
-    class AdditionalController < ApplicationController
-      def index
-        render json: 'Hello Git'
-      end
-    end
-  end
-end
--- api/app/controllers/api/v1/hello_controller.rb
+++ api/app/controllers/api/v1/hello_controller.rb
@@ -2,7 +2,6 @@ module Api
   module V1
     class HelloController < ApplicationController
       def index
-        puts "ADD COMMENT"
         render json: 'Hello Rails'
       end
     end
